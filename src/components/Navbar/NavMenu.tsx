7acf2a94d99b3437dcec9a9c45ed5d3b871d06eb
Said <said.kharboutli5@gmail.com>
2024-02-08 23:05:26 -0500

    feat: responsive and mobile sticky navbar

--- src/components/Navbar/NavMenu.tsx
+++ src/components/Navbar/NavMenu.tsx
@@ -4,10 +4,44 @@ type INavMenuProps = {
   children: ReactNode;
 };
 
-const NavMenu = (props: INavMenuProps) => (
-  <nav>
-    <ul className="flex gap-x-6 font-bold text-gray-200">{props.children}</ul>
-  </nav>
-);
+const NavMenu = (props: INavMenuProps) => {
+  return (
+    <>
+      <nav className="hidden md:block">
+        <ul className="flex gap-x-6 font-bold text-gray-200">
+          {props.children}
+        </ul>
+      </nav>
 
+      <div className="md:hidden">
+        <div className="peer/parent relative z-40 flex h-[20px] w-[25px] items-center  justify-center overflow-hidden p-0.5 transition-all duration-200">
+          <div className="flex size-full origin-center flex-col justify-between overflow-hidden transition-all duration-300">
+            <input
+              className="peer absolute left-0 top-0 z-50 size-full cursor-pointer opacity-0"
+              type="checkbox"
+              id="nav-toggle"
+            />
+            <div className="h-[2px] origin-left bg-white transition-all duration-300 peer-checked:-translate-x-10"></div>
+            <div className="h-[2px] rounded bg-white transition-all delay-75 duration-300 peer-checked:-translate-x-10"></div>
+            <div className="h-[2px] origin-left bg-white transition-all delay-150 duration-300 peer-checked:-translate-x-10"></div>
+
+            <div className="absolute top-2.5 flex w-0 items-center justify-between transition-all duration-500 translate-x-10 peer-checked:w-12 peer-checked:translate-x-0">
+              <div className="absolute h-[2px] w-[20px] bg-white transition-all delay-300 duration-500 rotate-0 [.peer:checked~*_&]:rotate-45"></div>
+              <div className="absolute h-[2px] w-[20px] bg-white transition-all delay-300 duration-500 -rotate-0 [.peer:checked~*_&]:-rotate-45"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="invisible fixed left-0 top-0 z-20 size-full bg-cod-900 opacity-0 transition-all duration-300 peer-has-[:checked]/parent:visible peer-has-[:checked]/parent:opacity-80" />
+        <div className="invisible fixed top-0 z-30 h-screen w-40 rounded-md p-8 text-right transition-all delay-100 duration-300 translate-x-16 peer-has-[:checked]/parent:visible peer-has-[:checked]/parent:-translate-x-24">
+          <nav className="mt-12">
+            <ul className="flex flex-col gap-y-6 font-bold text-gray-200">
+              {props.children}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
 export { NavMenu };
--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -3,32 +3,33 @@ import { Logo } from '@/components/Navbar/Logo';
 import { NavbarTwoColumns } from '@/components/Navbar/NavbarTwoColumns';
 import { NavMenu } from '@/components/Navbar/NavMenu';
 import { NavMenuItem } from '@/components/Navbar/NavMenuItem';
-import { Section } from '@/components/Section';
 
 const Navbar = () => (
-  <Section>
-    <NavbarTwoColumns>
-      <a href="/">
-        <Logo
-          icon={
-            <img
-              className="mr-2 w-10 rounded-full"
-              src="/images/site/profile_no_bg.png"
-            ></img>
-          }
-          name={<GradientText>Sa'id Kharboutli</GradientText>}
-        />
-      </a>
+  <div className="sticky top-0 z-30 mx-auto mb-3 w-full overflow-hidden rounded-b-md bg-cod-900/90 px-8 py-3 md:relative md:max-w-screen-lg md:bg-transparent md:px-3 md:py-6 4k:max-w-screen-xl">
+    <div className="mx-auto max-w-screen-lg">
+      <NavbarTwoColumns>
+        <a href="/">
+          <Logo
+            icon={
+              <img
+                className="mr-2 w-10 rounded-full"
+                src="/images/site/profile_no_bg.png"
+              ></img>
+            }
+            name={<GradientText>Sa'id Kharboutli</GradientText>}
+          />
+        </a>
 
-      <NavMenu>
-        <NavMenuItem href="/blog/">Blog</NavMenuItem>
-        <NavMenuItem href="/projects/">Projects</NavMenuItem>
-        <NavMenuItem href="/reviews/">Reviews</NavMenuItem>
-        <NavMenuItem href="/about-me/">Me</NavMenuItem>
-        <NavMenuItem href="/resume/">Resume</NavMenuItem>
-      </NavMenu>
-    </NavbarTwoColumns>
-  </Section>
+        <NavMenu>
+          <NavMenuItem href="/blog/">Blog</NavMenuItem>
+          <NavMenuItem href="/projects/">Projects</NavMenuItem>
+          <NavMenuItem href="/reviews/">Reviews</NavMenuItem>
+          <NavMenuItem href="/about-me/">Me</NavMenuItem>
+          <NavMenuItem href="/resume/">Resume</NavMenuItem>
+        </NavMenu>
+      </NavbarTwoColumns>
+    </div>
+  </div>
 );
 
 export { Navbar };
